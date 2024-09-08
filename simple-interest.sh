b6ad0ea02e295feb20bc85861e5085c6f93a265e
LavetiSandeep <sandeeplaveti835@gmail.com>
2024-09-08 12:37:13 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,9 @@
-echo 
+echo "enter the value of principal"
+read p
+echo "enter the rate of interest per year"
+read r
+echo "enter the time period in years"
+read t
+s= `expr $p \* $t \* $r /100`
+echo "the simple interest is:"
+echo $s
